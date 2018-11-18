970fb80dfb4ad7a40916daaa322010ec414cc3c8
Stephanie Tang <steptang@seas.upenn.edu>
2018-11-18 09:33:28 -0500

    feature detection added

---
+++ code/getFeatures.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+# returns NxF matrix with N row coordinates of the features across F objects
+# returns NxF matrix with N col coordinates of the features across F objects
+def getFeatures(img, bbox, maxCorners, imgSize, minDistance):
+
+	first = True
+	for x0, y0, w, h in bbox:
+		window = img[y0:y0+h, x0:x0+w]
+		corners = np.int0(cv2.goodFeaturesToTrack(window, maxCorners, 0.01, imgSize, minDistance))
+
+		x_f = [corner[0][0]+x0 for corner in corners]
+		y_f = [corner[0][1]+y0 for corner in corners]
+
+		x_f = np.vstack(np.pad(x_f, (0, maxCorners-len(x)), 'constant', constant_values=-1))
+		y_f = np.vstack(np.pad(y_f, (0, maxCorners-len(y)), 'constant', constant_values=-1))
+
+		if first:
+			x = x_f
+			y = y_f
+			first = False
+		else:
+			x = np.append(x, x_f, axis=1)
+			y = np.append(y, y_f, axis=1)
+
+		return x, y
