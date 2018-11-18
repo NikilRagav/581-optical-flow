b896b6c5099cd3dca29f13e342897138659702d9
Stephanie Tang <steptang@seas.upenn.edu>
2018-11-18 09:52:08 -0500

    finished bounding boxes

--- code/getBoundingBox.py
+++ code/getBoundingBox.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
 
-def getBoundingBox():
+def getBoundingBox(img):
+	bbox = cv2.selectROI(im)
 
+	# imCrop = im[int(bbox[1]):int(bbox[1]+bbox[3]), int(bbox[0]):int(bbox[0]+bbox[2])]
+	# cv2.imshow("Image", imCrop)
+    # cv2.waitKey(0)
 	return bbox
